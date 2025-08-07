9fb10c2fc3649b937036d1346eabbb3e19789925
SanjayBista1010 <sanjaybista1010@gmail.com>
2025-08-08 00:33:44 +1000

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    author='Sanjay',
+    author_email='sanjaybista1010@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas','numpy','seaborn']
+
+)
