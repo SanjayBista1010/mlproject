aba7ef3e773e8422e214009ce8bd95a457948d27
SanjayBista1010 <sanjaybista1010@gmail.com>
2025-08-08 13:37:31 +1000

    Added logging and exception handling modules

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 from setuptools import find_packages, setup
+from typing import List
 
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+        This funtion will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace ('\n','') for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 setup(
     name='mlproject',
@@ -7,6 +22,6 @@ setup(
     author='Sanjay',
     author_email='sanjaybista1010@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas','numpy','seaborn']
+    install_requires=get_requirements('requirements.txt')
 
 )
