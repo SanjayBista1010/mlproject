3b38e2a8814c344826dd08d95072f4d897d98cc5
SanjayBista1010 <sanjaybista1010@gmail.com>
2025-08-09 23:06:58 +1000

    Hyperparameter Training

--- src/utils.py
+++ src/utils.py
@@ -1,31 +1,42 @@
 import os
 import sys
 
-import numpy as np
+import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
-    """Saves an object to a file using dill."""
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
-            
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
-
-def evaluate_models(models, X_test, y_test, X_train, y_train):
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
 
-            model.fit(X_train, y_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
 
             y_train_pred = model.predict(X_train)
 
@@ -36,8 +47,16 @@ def evaluate_models(models, X_test, y_test, X_train, y_train):
             test_model_score = r2_score(y_test, y_test_pred)
 
             report[list(models.keys())[i]] = test_model_score
-        
+
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
